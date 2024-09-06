2e63914d597e5675067943676cb180130ce47391
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-09-06 08:51:13 +0300

    basit logic

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -3,12 +3,20 @@ extends Node2D
 
 var greetings = "Hello"
 const firstName = "Anil"
+const favNum = 37
 
 func _ready() -> void:
 	print(greetings) # Hello
 	greetingAnil() # Hello anil!!!
 	var number = storeNum(23)
 	print(number) # 23
+	
+	if favNum == 37:
+		print("it is 37")
+	elif favNum == 99:
+		print("not even close")
+	else:
+		print("none")
 
 #func _process(delta: float) -> void:
 	#pass
