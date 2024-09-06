8784f39d589fde5af575579269c790774dc53db7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-09-06 08:41:06 +0300

    function returns int

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -4,13 +4,17 @@ extends Node2D
 var greetings = "Hello"
 const firstName = "Anil"
 
-
 func _ready() -> void:
 	print(greetings) # Hello
 	greetingAnil() # Hello anil!!!
+	var number = storeNum(23)
+	print(number) # 23
 
 #func _process(delta: float) -> void:
 	#pass
 
 func greetingAnil():
 	print("Hello anil!!!")
+
+func storeNum(num : int) -> int:
+	return num
