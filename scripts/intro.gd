22dcd5dfe0c5dc657b7a30c96c56ba342dbce581
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-09-06 08:36:20 +0300

    gdscript intro

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+var greetings = "Hello"
+const firstName = "Anil"
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	print(greetings) # Hello
+	greetingAnil() # Hello anil!!!
 
+#func _process(delta: float) -> void:
+	#pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func greetingAnil():
+	print("Hello anil!!!")
